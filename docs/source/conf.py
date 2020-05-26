7e991d90eff1a8cb7c849bea51485356c2600a29
Filip Mivalt <mivalt.filip@gmail.com>
2020-05-26 15:35:04 -0500

    v0.0.1 docs formatting

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ except:
 # General information about the project.
 project = 'Automated iEEG Sleep Classifier (AISC)'
 author = 'Filip Mivalt'
-copyright = '2020, Bioelectronics Neurophysiology and Engineering Lab - Mayo Clinic '
+copyright = '2020, Bioelectronics Neurophysiology and Engineering Lab - Mayo Clinic. \n'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -109,13 +109,14 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#html_theme = 'sphinx_rtd_theme'
-html_theme = 'default'
+html_theme = 'sphinx_rtd_theme'
+#html_theme = 'default'
 
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
+
 html_theme_options = {
     'canonical_url': '',
     'analytics_id': 'UA-167677696-1',  #  Provided by Google in your dashboard
@@ -123,7 +124,7 @@ html_theme_options = {
     'display_version': True,
     'prev_next_buttons_location': 'bottom',
     'style_external_links': True,
-    'style_nav_header_background': 'white',
+    'style_nav_header_background': '',
     # Toc options
     'collapse_navigation': False,
     'sticky_navigation': True,
@@ -138,10 +139,10 @@ html_theme_options = {
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+html_title = 'Automated iEEG Sleep Classifier'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-html_short_title = None
+html_short_title = 'AISC'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
@@ -155,7 +156,7 @@ html_short_title = None
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-#html_static_path = ['_static']
+html_static_path = ['_static']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
@@ -190,10 +191,10 @@ html_short_title = None
 #html_show_sourcelink = True
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
-#html_show_sphinx = True
+html_show_sphinx = False
 
 # If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
-#html_show_copyright = True
+html_show_copyright = True
 
 # If true, an OpenSearch description file will be output, and all pages will
 # contain a <link> tag referring to it.  The value of this option must be the
@@ -204,7 +205,7 @@ html_short_title = None
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'ReadtheDocsTemplatedoc'
+htmlhelp_basename = 'AISC'
 
 
 # -- Options for LaTeX output ---------------------------------------------
